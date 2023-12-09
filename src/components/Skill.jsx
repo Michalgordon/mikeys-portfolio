c46d9e5d42b29e9bea788ed8c72d583afd51cb49
Mikey <michalgordon92@gmail.com>
2023-12-09 14:45:02 -0800

    added rotation to bolts

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,20 +1,28 @@
 import PropTypes from "prop-types";
 import Icon from "./Icon";
-//todo add rotation to bolts or some animation?
 const Skill = ({ title, level }) => {
+  const renderIcon = (index) => {
+    const rotateRules = {
+      1: ["rotate-0"],
+      2: ["-rotate-20 mb-1", "rotate-20 mb-1"],
+      3: ["-rotate-35", "rotate-0 mb-1", "rotate-35"],
+    };
+    const rotateAccordingToIndex = rotateRules[level][index];
+    return (
+      <Icon
+        key={index}
+        className={` w-2 ${rotateAccordingToIndex}`}
+        fill="orange"
+        name="bolt"
+      />
+    );
+  };
+
   return (
-    <div className="inline-flex items-center gap-1">
+    <div className="inline-flex items-center gap-1 mx-4">
       <div className="flex items-center justify-center">
         {[...Array(level).keys()].map((index) => {
-          let rotate = "[45deg]";
-          return (
-            <Icon
-              key={index}
-              className={`relative w-2 rotate-${rotate}`}
-              fill="orange"
-              name="bolt"
-            ></Icon>
-          );
+          return renderIcon(index);
         })}
       </div>
       <h1 className="capitalize font-medium text-sm whitespace-nowrap	">
